d1b7ef517c396b5508a74fec23ff15107e97f0f7
ovechka23 <128913063+ovechka23@users.noreply.github.com>
2023-05-22 12:49:14 +0300

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -1,38 +1,24 @@
-#ifndef _MATRIX_H
-#define _MYATRIX_H
-
-#include <iostream>
-#include <math.h>
-using namespace std;
-
-class Matrix
+#pragma once
+#include"operations.h"
+#include<iostream>
+#include<vector>
+class matrix :public  Operations
 {
-protected:
-	double** m;
-	int i, j;
-	void Get_mem(int _i, int _j);
+	int SizeRov;
+	int SizeCol;
+	double value;
+	bool isNum;
+	std::vector<std::vector<double>> array;
+
 public:
-	Matrix(int _i, int _j);
-	Matrix(const Matrix& m1);
-	~Matrix();
-	static inline void Error(const char* str);
-	friend Matrix operator+(Matrix m1, Matrix m2);
-	friend Matrix operator-(Matrix m1, Matrix m2);
-	friend Matrix operator*(Matrix m1, Matrix m2);
-	friend Matrix operator/(Matrix m1, Matrix m2);
-	friend Matrix operator+(Matrix& m1, double a);
-	friend Matrix operator-(Matrix m1, double a);
-	friend Matrix operator*(Matrix m1, double a);
-	friend Matrix operator/(Matrix m1, double a);
-	friend Matrix operator+(double a, Matrix m1);
-	friend Matrix operator-(double a, Matrix m1);
-	friend Matrix operator*(double a, Matrix m1);
-	friend Matrix operator/(double a, Matrix m1);
-	double* operator[](const int k);
-	Matrix const& operator=(Matrix const& m1);
-	void Add();
+	matrix();
+	~matrix();
+	matrix(const std::string& str);
+	matrix(const Operations*);
+	Operations* add(const Operations*); // =+
+	Operations* increase(const Operations*); // =*
+	Operations* subtraction(const Operations*); // =-
+	Operations* div(const Operations*); // =/
 
-	void Print() const;
-
+	friend std::ostream& operator <<(std::ostream& out, const matrix& num);
 };
-#endif
